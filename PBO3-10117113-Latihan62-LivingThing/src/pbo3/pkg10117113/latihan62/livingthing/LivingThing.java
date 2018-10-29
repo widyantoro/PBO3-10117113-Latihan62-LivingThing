d1e29a8f31d14fcc05a815297d45652b944c9918
widyantoro <43356973+widyantoro@users.noreply.github.com>
2018-10-30 04:58:56 +0700

    Add files via upload

---
+++ PBO3-10117113-Latihan62-LivingThing/src/pbo3/pkg10117113/latihan62/livingthing/LivingThing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan62.livingthing;
+
+/*
+ * Nama     : Eka Widyantoro
+ * NIM      : 10117113
+ * Kelas    : IF-3/PBO-3
+ * Desc     : Program untuk menampilkan kegiatan manusia
+ *              
+ *            
+ */
+public abstract class LivingThing {
+    public abstract void walk(String nama);
+    public void breath(String nama){
+        System.out.println("Bernafas"); 
+    }
+    public void eat (String nama){
+        System.out.println("Makan");    
+    }
+}
---
+++ PBO3-10117113-Latihan62-LivingThing/src/pbo3/pkg10117113/latihan62/livingthing/PBO310117113Latihan62LivingThing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan62.livingthing;
+
+/*
+ * Nama     : Eka Widyantoro
+ * NIM      : 10117113
+ * Kelas    : IF-3/PBO-3
+ * Desc     : Program untuk menampilkan kegiatan manusia
+ *              
+ *            
+ */
+public class PBO310117113Latihan62LivingThing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setNama("Rizki Adam Kurniawan");
+        System.out.print(human.getNama()+" ");
+        human.walk("");
+        System.out.print(human.getNama()+" ");
+        human.breath("");
+        System.out.print(human.getNama()+" ");
+        human.eat("");
+    }
+    
+}
